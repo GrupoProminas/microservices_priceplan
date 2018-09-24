cb7fecd4d7667c34424c5ffdbc40e438f2820a78
Paulo Ferreira <zenmidia@gmail.com>
2018-09-24 16:00:38 -0300

    Update List Price Course

--- src/api/pricesCourses/read.js
+++ src/api/pricesCourses/read.js
@@ -8,28 +8,15 @@ const listPrices = (req, res) => {
      * Find all registers of Prices collection
      */
     PriceCourses
-        .paginate(
-            [
-                {
-                    $match: req.query.where
-                }
-            ],
-            req.query.limit,
-            req.query.page
-        )
+        .paginate(req.query.aggregate, req.query.limit, req.query.page)
         .then(result => {
-
-            // If no have data send a not found response
-            if (!result.data.length) {
-                return res.api.send(null, res.api.codes.OK);
-            }
+            if (!result.data.length) return res.api.send(null, res.api.codes.NOT_FOUND);
 
             return res.api.send(result.data, res.api.codes.OK, {paginate: result.paginate});
         })
         .catch(err => {
-            return res.api.send(err.message, res.api.codes.INTERNAL_SERVER_ERROR);
+            return res.api.send(err, res.api.codes.INTERNAL_SERVER_ERROR);
         })
-
 
 };
 
