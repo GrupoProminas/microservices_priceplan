46d72fabb08ec0abc01ed2f2bb9c783dbe8717be
wanbe <wanber@outlook.com>
2020-09-15 11:04:04 -0300

    fix update voucher

--- src/api/vouchers/update.js
+++ src/api/vouchers/update.js
@@ -1,22 +1,30 @@
 /* eslint-disable id-length */
-import {models} from 'mongoose';
+import {models, Types} from 'mongoose';
 
 const {Vouchers} = models;
 
 const updateVouchers = async (req, res) => {
 
-    try {
-        const voucher = await Vouchers.findById(req.params._id);
+    if (req.body.isActive === '' || req.body.isActive === undefined) delete req.body.isActive;
+    if (req.body.userType === 'system') delete req.body.cpf;
 
-        voucher.overwrite(req.body);
+    const update = {
+        $set: req.body,
+        $unset: {}
+    };
 
-        await voucher.save();
+    if (!('_courseId' in req.body) || !req.body._courseId) update.$unset._courseId = 1; else update.$set.tags = [];
+    if (req.body.userType === 'system') update.$unset.cpf = 1;
 
-        return res.api.send(voucher, res.api.codes.OK);
+    if (Object.keys(update.$set).length === 0) delete update.$unset;
 
-    } catch (err) {
-        return res.api.send(err.stack, res.api.codes.INTERNAL_SERVER_ERROR);
-    }
+    return Vouchers
+        .findOneAndUpdate(
+            {_id: Types.ObjectId(req.params._id)},
+            update
+        )
+        .then(updated => res.api.send(updated, res.api.codes.OK))
+        .catch(err => res.api.send(err.stack, res.api.codes.INTERNAL_SERVER_ERROR));
 };
 
 export default updateVouchers;
