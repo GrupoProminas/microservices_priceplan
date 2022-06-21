fbf12cad7f82b92e4d081e2342f522b51e21d60d
henriquemalvar <henriquemalvar@gmail.com>
2022-06-21 08:56:37 -0300

    add combos model and crud

---
+++ src/api/combos/_validates/create.validate.js
@@ -0,0 +1,118 @@
+import Joi from 'joi';
+
+export default (req, res, next) => {
+    Joi.object({
+        maxCourses: Joi.number().required(),
+        certifier: Joi.string().required(),
+        courseType: Joi.string().required(),
+        area: Joi.string().allow('*'),
+        tags: Joi.array().items(Joi.string()),
+        subcategory: Joi.array().items(Joi.string())
+            .allow('*'),
+        _courses: Joi.array().items(Joi.string()),
+        _exceptions: Joi.array().items(Joi.string()),
+        blackListedTags: Joi.array().items(Joi.string()),
+        commissionLevel: Joi.string().valid('min', 'med', 'max')
+            .required(),
+        paymentPlan: Joi.array().items(
+            Joi.object({
+                courses: Joi.number().required(),
+                plan: Joi.object({
+                    creditCard: Joi.array().items(
+                        Joi.object({
+                            _id: Joi.boolean().allow(false),
+                            installment: Joi.number().required(),
+                            value: Joi.number().required()
+                        })
+                    ),
+                    debitCard: Joi.array().items(
+                        Joi.object({
+                            _id: Joi.boolean().allow(false),
+                            installment: Joi.number().required(),
+                            value: Joi.number().required()
+                        })
+                    ),
+                    cardRecurrence: Joi.array().items(
+                        Joi.object({
+                            _id: Joi.boolean().allow(false),
+                            installment: Joi.number().required(),
+                            value: Joi.number().required()
+                        })
+                    ),
+                    boleto: Joi.array().items(
+                        Joi.object({
+                            _id: Joi.boolean().allow(false),
+                            installment: Joi.number().required(),
+                            value: Joi.number().required()
+                        })
+                    ),
+                    pix: Joi.array().items(
+                        Joi.object({
+                            _id: Joi.boolean().allow(false),
+                            installment: Joi.number().required(),
+                            value: Joi.number().required()
+                        })
+                    )
+                })
+            })
+        ),
+        minPaymentPlan: Joi.array().items(
+            Joi.object({
+                courses: Joi.number().required(),
+                plan: Joi.object({
+                    creditCard: Joi.array().items(
+                        Joi.object({
+                            _id: Joi.boolean().allow(false),
+                            installment: Joi.number().required(),
+                            value: Joi.number().required()
+                        })
+                    ),
+                    debitCard: Joi.array().items(
+                        Joi.object({
+                            _id: Joi.boolean().allow(false),
+                            installment: Joi.number().required(),
+                            value: Joi.number().required()
+                        })
+                    ),
+                    cardRecurrence: Joi.array().items(
+                        Joi.object({
+                            _id: Joi.boolean().allow(false),
+                            installment: Joi.number().required(),
+                            value: Joi.number().required()
+                        })
+                    ),
+                    boleto: Joi.array().items(
+                        Joi.object({
+                            _id: Joi.boolean().allow(false),
+                            installment: Joi.number().required(),
+                            value: Joi.number().required()
+                        })
+                    ),
+                    pix: Joi.array().items(
+                        Joi.object({
+                            _id: Joi.boolean().allow(false),
+                            installment: Joi.number().required(),
+                            value: Joi.number().required()
+                        })
+                    )
+                })
+            })
+        ),
+        isEnabled: Joi.boolean().required(),
+        name: Joi.string(),
+        quantityCourses: Joi.array().items(
+            Joi.object({
+                typeCourse: Joi.string().required(),
+                quantity: Joi.number().required(),
+                subcategory: Joi.string().allow('*')
+            })
+        )
+    }).validate(req.body, err => {
+        if (err) {
+            return res.status(400).json({
+                error: err.details[0].message
+            });
+        }
+        next();
+    });
+};
---
+++ src/api/combos/read.js
@@ -0,0 +1,20 @@
+
+
+const listCombos = (req, res) => {
+
+const {Combos} = req.models;
+
+
+    Combos
+        .paginate(req.query.aggregate, req.query.limit, req.query.page)
+        .then(result => {
+            if (!result.data.length) return res.api.send(null, res.api.codes.NOT_FOUND);
+
+            return res.api.send(result.data, res.api.codes.OK, {paginate: result.paginate});
+        })
+        .catch(err => {
+            return res.api.send(err, res.api.codes.INTERNAL_SERVER_ERROR);
+        });
+};
+
+export default listCombos;
