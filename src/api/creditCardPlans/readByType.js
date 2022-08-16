768ac2645a4e0e914fd05a50ea1e42978a44dc67
lucaspauloop <lucaspaulodeoliveira@outlook.com>
2022-08-16 08:16:05 -0300

    add selectParcel

--- src/api/creditCardPlans/readByType.js
+++ src/api/creditCardPlans/readByType.js
@@ -1,27 +1,44 @@
+/* eslint-disable max-statements */
+import { Types } from 'mongoose';
 import CreditCardPlansService from '../../services/CREDITCARDPLANS/CreditCardPlans.service';
 
+const getEnrolment = async (req) => {
+    const {Enrolments} = req.models;
 
-const readByCertifier = (req, res) => {
+    const enrolmentId = req.query.enrolmentId ? req.query.enrolmentId : '';
 
-const {CreditCardPlans} = req.models;
+    if (!enrolmentId) return false;
 
+    return Enrolments.findOne({_id: Types.ObjectId(enrolmentId)});
+}
 
-    CreditCardPlans
-        .findOne({
-            _certifierName: decodeURIComponent(req.params.certifier),
-            _typeName: decodeURIComponent(req.params._typeName),
-            isActive: true,
-            _type: decodeURIComponent(req.params._type)
-        },
-        {
-            paymentPlan: 1,
-            _id: 0
+const readByCertifier = async (req, res) => {
+
+    const {CreditCardPlans} = req.models;
+
+    getEnrolment(req)
+        .then(enrolment => {
+            if (enrolment) req.enrolment = enrolment;
+
+            return CreditCardPlans
+            .findOne({
+                _certifierName: decodeURIComponent(req.params.certifier),
+                _typeName: decodeURIComponent(req.params._typeName),
+                isActive: true,
+                _type: decodeURIComponent(req.params._type)
+            },
+            {
+                paymentPlan: 1,
+                _id: 0
+            });
         })
         .then(installmentArray => {
+            const totalArray  = req.params.total.split(',');
+            let selectParcels = 1;
+            const chargeType  = decodeURIComponent(req.params._type);
+            let total         = 0;
+            let charges       = 1;
 
-            const totalArray = req.params.total.split(',');
-            let total = 0;
-            let charges = 1;
             if (totalArray.length === 1) {
                 total = totalArray[0];
             } else if (totalArray.length === 2) {
@@ -33,9 +50,16 @@ const {CreditCardPlans} = req.models;
 
             if (!installmentArray) return res.api.send(null, res.api.codes.NOT_FOUND);
 
-            const creditCardPlansService = new CreditCardPlansService(req.models);
-            const result = creditCardPlansService.calcCardPlanforPayment(installmentArray, total, charges);
+            if (req.enrolment && req.enrolment.metadata && req.enrolment.metadata.prices) {
+                if (chargeType === 'rate-enrolment' && req.enrolment.metadata.prices.enrolment) {
+                    selectParcels = parseInt(req.enrolment.metadata.prices.enrolment.installments);
+                } else if ((chargeType === 'monthly' || chargeType === 'rate-enrolment-monthly') && req.enrolment.metadata.prices.course) {
+                    selectParcels = parseInt(req.enrolment.metadata.prices.course.installments);
+                }
+            }
 
+            const creditCardPlansService = new CreditCardPlansService(req.models);
+            const result = creditCardPlansService.calcCardPlanforPayment(installmentArray, total, charges, selectParcels);
 
             if (!result) return res.api.send(null, res.api.codes.NOT_FOUND);
 
